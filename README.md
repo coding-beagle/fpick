6e389dc94db9bf705126859225c6f589666629bc
coding-beagle <nicholas.teague@focusrite.com>
2026-05-17 07:02:28 +0100

    update readme, add more basic ux

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-This is a template repository to make it so Coding Beagle has to do less work when creating a Python CLI app via Click and Pytest.
+# FPick
 
-Do `make` to check what the existing rulesets are, and modify the rest!
+FPick is a simple utility that allows users to access their OS file dialog from the CLI.
+
+This is particularly useful for image / video browsing with scripts.
+
+## Example usage
+
+Running a python script that takes an image or video file as an output:
+
+```
+python some_script.py `fpick`
+```
+
+![](./readme_resources/first_demo.gif)
--- fpick/__main__.py
+++ fpick/__main__.py
@@ -12,10 +12,38 @@ FPICK_CACHE = "fpickcache"  # previous directory
 LAST_DIR_KEY = "last_dir"
 
 
+def file_extension_string_to_arg(ext_string: str) -> tuple[str, str]:
+    """Check if user has put a . before the ext string, and also create a label"""
+    stripped_string = ext_string.strip()
+
+    if stripped_string.startswith("."):
+        label: str = stripped_string.split(".")[-1]
+
+        return (f"{label.upper()} Files (*{stripped_string})", stripped_string)
+    else:
+        label: str = stripped_string
+        return (
+            f"{label.upper()} Files (*{stripped_string.lower()})",
+            f".{stripped_string.lower()}",
+        )
+
+
 @click.command()
-@click.option("--directory", "-d", is_flag=True, default=False)
-def cli(directory):
-    """Open the file dialog and write it to stdout, and cache the folder of the last selected file"""
+@click.option(
+    "--directory",
+    "-d",
+    is_flag=True,
+    default=False,
+    help="Select directories instead of files",
+)
+@click.option(
+    "--file_extension",
+    "-f",
+    default=None,
+    help="File extension to filter by",
+)
+def cli(directory, file_extension):
+    """Return a selected file to stdout"""
 
     with shelve.open(FPICK_CACHE) as fcache:
         last_dir = fcache.get(LAST_DIR_KEY, None)
@@ -29,7 +57,14 @@ def cli(directory):
             file_path: str = filedialog.askdirectory(initialdir=last_dir)
             to_cache = file_path
         else:
-            file_path: str = filedialog.askopenfilename(initialdir=last_dir)
+
+            if file_extension:
+                file_path: str = filedialog.askopenfilename(
+                    initialdir=last_dir,
+                    filetypes=[file_extension_string_to_arg(file_extension)],
+                )
+            else:
+                file_path: str = filedialog.askopenfilename(initialdir=last_dir)
             to_cache = os.path.dirname(file_path)
 
         click.echo(file_path)
